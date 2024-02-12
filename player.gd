049cc4a95fba385a646071078bdf7dfb67832a22
lichen014 <121971822+lichen014@users.noreply.github.com>
2024-02-12 13:40:26 -0800

    started on part 5 12:05

--- player.gd
+++ player.gd
@@ -46,7 +46,7 @@ func handle_jump():
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = movement_data.jump_velocity
-	if not is_on_floor():
+	elif not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
 			
