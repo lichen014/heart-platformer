adfaa1e6e1f30012187755d27fdda1ea1f0d9b8f
lichen014 <121971822+lichen014@users.noreply.github.com>
2024-02-15 13:53:54 -0800

    finished part 5

--- player.gd
+++ player.gd
@@ -8,6 +8,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var starting_position = global_position
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -82,3 +83,7 @@ func update_animations(input_axis):
 		
 	if not is_on_floor():
 		animated_sprite_2d.play("jump")
+
+
+func _on_hazard_detector_area_entered(area):
+	global_position = starting_position
