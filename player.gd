70ebb950a29ef0aef985967f6a90c0548a28fafb
lichen014 <121971822+lichen014@users.noreply.github.com>
2024-02-16 14:07:48 -0800

    video 7 completed

--- player.gd
+++ player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta):
 	handle_wall_jump()
 	handle_jump()
 	var input_axis = Input.get_axis("ui_left", "ui_right")
-	handle_acceleration(input_axis, delta) 
+	handle_acceleration(input_axis, delta)
 	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
@@ -30,7 +30,7 @@ func _physics_process(delta):
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta
-		
+
 func handle_wall_jump():
 	if not is_on_wall_only(): return
 	var wall_normal = get_wall_normal()
@@ -38,52 +38,48 @@ func handle_wall_jump():
 		velocity.x = wall_normal.x * movement_data.speed
 		velocity.y = movement_data.jump_velocity
 		just_wall_jumped = true
-		
-		
 
 func handle_jump():
 	if is_on_floor(): air_jump = true
 	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = movement_data.jump_velocity
 	elif not is_on_floor():
-		if Input.is_action_just_released("ui_accept") and velocity.y < movement_data.jump_velocity / 2:
+		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
-			
+		
 		if Input.is_action_just_pressed("ui_up") and air_jump and not just_wall_jumped:
 			velocity.y = movement_data.jump_velocity * 0.8
 			air_jump = false
-			
+
 func handle_acceleration(input_axis, delta):
 	if not is_on_floor(): return
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
-		
+
 func handle_air_acceleration(input_axis, delta):
 	if is_on_floor(): return
 	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta) 
-
-		
-func apply_air_resistance(input_axis, delta):
-	if input_axis == 0 and not is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
 
 func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
-		
+
+func apply_air_resistance(input_axis, delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
+
 func update_animations(input_axis):
 	if input_axis != 0:
 		animated_sprite_2d.flip_h = (input_axis < 0)
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
-		
+	
 	if not is_on_floor():
 		animated_sprite_2d.play("jump")
-
 
 func _on_hazard_detector_area_entered(area):
 	global_position = starting_position
